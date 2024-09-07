41f701586f1f5d0e7da3a6a06f565419d1deee6b
elicordero111 <eli_gabriel_cordero_f@live.com>
2024-09-07 01:33:13 -0600

    Add - Proyecto base HTML basico con vite y tailwind preconfigurado

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // O la ruta a tu archivo HTML principal
+    "./src/**/*.{html,js}", // Ajusta esto según tu framework o si usas otros tipos de archivo
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "./src", // Cambia esto a la ruta donde se encuentra tu index.html
+});
